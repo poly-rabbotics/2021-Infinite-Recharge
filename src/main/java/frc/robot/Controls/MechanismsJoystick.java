d1409071e994e1a0bc8a8e7196388b160004d632
rohancreddy9 <65629520+rohancreddy9@users.noreply.github.com>
2021-02-27 15:07:30 -0800

    Added encoder test and transferred older code

---
+++ src/main/java/frc/robot/Controls/MechanismsJoystick.java
@@ -0,0 +1,14 @@
+package frc.robot.Controls;
+
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.RobotMap;
+
+public class MechanismsJoystick {
+    private static Joystick joystick = RobotMap.mechanismsJoystick;
+
+
+    public static boolean startGalacticSearch(){
+    return  joystick.getRawButton(4); //needs to be given an actual number
+      }
+
+}
